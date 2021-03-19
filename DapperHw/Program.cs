0c98b5ff58eff1902ad5a463b436e2e779517241
unknown <nellysherova@mail.ru>
2021-03-19 12:36:43 +0300

    Создала класс User

--- DapperHw/Program.cs
+++ DapperHw/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.SqlClient;
 namespace DapperHw
 {
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+    public static class Commands
+    {
+        
+    }
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
